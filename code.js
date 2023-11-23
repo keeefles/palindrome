bdf710fe3fa041dcd76978a0d95db8a5adef96ec
Aakeefah Jones <aakeefahj@gmail.com>
2023-11-23 14:16:12 +0200

    exercise

---
+++ code.js
@@ -0,0 +1,24 @@
+/*
+=== String challenge ====
+Palindrome: a word, phrase, or sequence that reads the same backward as forwards, e.g. madam
+- Create a repo called a palindrome.
+- Open your cloned repo into a VS code
+- Create a function called isPalindrome that will take a single parameter and return a string value.
+The output should look like: madam is a palindrome if the user passes madam as an argument.
+NB: When creating a function, we refer to the variable between the parenthesis as a parameter.
+When we call or invoke a function, we refer to it as an argument.
+*/
+
+function isPalindrome(string) {
+
+    let lowerCase = string.toLowerCase();
+    let reverse = lowerCase.split('').reverse().join('');
+
+    if(lowerCase == reverse) {
+      return `${string} is a palindrome.`;
+    }else {
+      return `${string} is not a palindrome.`;
+    }
+  }
+let result = isPalindrome("Madam");
+console.log(result)
